fbf2ee64be2a7fb6e76cd766db0393dfa66ada74
Rabab Zaitouni <rabab.zaitouni@gmail.com>
2020-04-06 19:39:05 +0100

    created ingredient model

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,3 +36,11 @@ end
 #   )
 #   puts "Created #{recipe.name}"
 # end
+
+puts 'Creating ingredients...'
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+puts 'Created ingredients'
