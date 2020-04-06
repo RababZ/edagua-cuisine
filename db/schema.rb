385faee29f75aa1d5eb01928e46ea3fc808f9442
Rabab Zaitouni <rabab.zaitouni@gmail.com>
2020-04-06 19:50:37 +0100

    created dose model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_181939) do
+ActiveRecord::Schema.define(version: 2020_04_06_184536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doses", force: :cascade do |t|
+    t.text "description"
+    t.bigint "ingredient_id", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_doses_on_recipe_id"
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -28,4 +38,6 @@ ActiveRecord::Schema.define(version: 2020_04_06_181939) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "doses", "ingredients"
+  add_foreign_key "doses", "recipes"
 end
