277aa786059fd7f8885100b7d9b8b92a23fa99d7
Rabab Zaitouni <rabab.zaitouni@gmail.com>
2020-04-09 20:19:36 +0100

    added uploaded image to the show

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,14 +1,14 @@
-const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.navbar-edagua');
-  if (navbar) {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-edagua-white');
-      } else {
-        navbar.classList.remove('navbar-edagua-white');
-      }
-    });
-  }
-}
+// const initUpdateNavbarOnScroll = () => {
+//   const navbar = document.querySelector('.navbar-edagua');
+//   if (navbar) {
+//     window.addEventListener('scroll', () => {
+//       if (window.scrollY >= window.innerHeight) {
+//         navbar.classList.add('navbar-edagua-white');
+//       } else {
+//         navbar.classList.remove('navbar-edagua-white');
+//       }
+//     });
+//   }
+// }
 
-export { initUpdateNavbarOnScroll };
+// export { initUpdateNavbarOnScroll };
