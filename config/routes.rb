2b88bf6f5d43087199c2388a91963bb9653744f1
Rabab Zaitouni <rabab.zaitouni@gmail.com>
2020-04-14 16:37:57 +0100

    added review model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :recipes, only: [:index, :show, :new, :create] do
     resources :doses, only: [:index, :new, :create]
+    resources :reviews, only: [:index, :new, :create]
   end
   resources :doses, only: [:destroy]
 end
