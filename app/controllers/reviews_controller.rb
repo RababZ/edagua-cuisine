1e0d74eaa0db9c9bb5ae9a4ab968bfa8132343b2
Rabab Zaitouni <rabab.zaitouni@gmail.com>
2020-04-14 18:18:41 +0100

    added reviews to recipes and displayed each review with stars

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @recipe = Recipe.find(params[:recipe_id])
+    @review.recipe = @recipe
+    if @review.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :recipe
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: 1..5,
+    message: 'Rating must be between 1 and 5'}
 end
